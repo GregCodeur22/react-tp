bd6d488811c8730ecedd10a56cf8d5d6a5898ca1
Grégory <gregory.arside@gmail.com>
2022-08-26 18:43:35 +0200

    liste react

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 
-
-
 const App = () => {
 
     //state
-    const [compteur, setCompteur] = useState(1)
+    const [fruits, setFruits] = useState([
+        {id: 1, nom:'abricot'},
+        {id: 2, nom:'banane'},
+        {id: 3, nom:'cerise'},
+    ])
 
     //comportement
-    const handleCLick = () => {
-        setCompteur(compteur + 1)
+    const handleDelete = (id) => {
+        console.log(id);
+
+
+        //copie du state
+        const fruitCopie = [...fruits];
+
+        //manipuler mon state copier
+        const fruitCopieUpdated = fruitCopie.filter((fruit) => fruit.id !== id)
+        
+        //modifier mon state avec le setter 
+        setFruits(fruitCopieUpdated)
     }
 
     //affichage
     return (
         <div>
-            <h1 > {compteur} </h1>
-            <button onClick={handleCLick}>Incrément</button>
+            <h1>liste de fruits</h1>
+            <ul>{fruits.map((fruit) => {
+                return <li key={fruit.id}> {fruit.nom} <button onClick={() => handleDelete(fruit.id)}>x</button> </li>
+            })}</ul>
         </div>
     );
 };
